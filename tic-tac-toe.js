764d846e6848c669e5602f9c9aa5198479ca6cd9
Sydonae England <sydonaeengland@gmail.com>
2024-10-27 20:34:56 -0500

    Added the hover effect over the squares if the mouse hovers over an empty square

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,34 +1,40 @@
-//Exercise 1 -Adding the 'square' class to each square using setAttribute
-document.addEventListener("DOMContentLoaded", function () {
-  const squares = document.querySelectorAll("#board div");
-
-  //Loop through each square to add the square
-  squares.forEach(function (square) {
-    //Adding the 'square' using setAttribute
-    square.setAttribute("class", "square");
-  });
-});
-
-//Exerise 2 - Placing 'X' or 'O' on a clicked square, alternating players, and updating the board status
 document.addEventListener("DOMContentLoaded", function () {
   const gameSquares = document.querySelectorAll("#board div");
   let player = "X";
-  let boardStatus = Array(9).fill(null); //Initializing an empty array
+  let boardStatus = Array(9).fill(null); // Initializing an empty array
 
+  // Loop through each game square to add the 'square' class
   gameSquares.forEach(function (square, index) {
+    // Adding the 'square' using setAttribute
+    square.setAttribute("class", "square");
+
+    // Exercise 2 - Placing 'X' or 'O' on a clicked square, alternating players, and updating the board status
     square.addEventListener("click", function () {
-      //Ensuring that the player can only add their mark if the square is empty
+      // Ensuring that the player can only add their mark if the square is empty
       if (!square.textContent) {
         // Adds X or O when the square is clicked
         square.textContent = player;
         square.setAttribute("class", `square ${player}`);
 
-        //Updating the boardStatus array
+        // Updating the boardStatus array
         boardStatus[index] = player;
 
-        //Switching between players
+        // Switching between players
         player = player === "X" ? "O" : "X";
       }
+    });
+
+    // Exercise 3 - Applying a hover style over an empty square
+    square.addEventListener("mouseenter", function () {
+      //  Applies the hover effect only if the square is empty
+      if (!square.textContent) {
+        square.classList.add("hover");
+      }
+    });
+
+    square.addEventListener("mouseleave", function () {
+      // Remove the hover effect when the mouse isnot over the empty square
+      square.classList.remove("hover");
     });
   });
 });
